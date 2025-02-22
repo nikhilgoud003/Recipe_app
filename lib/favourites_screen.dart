5457afaa70765966c75cc94b67857256f2810f0f
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-02-21 22:21:59 -0500

    New component favorites items was added

--- lib/favourites_screen.dart
+++ lib/favourites_screen.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
-import 'details_screen.dart';
 import 'main.dart';
+import 'details_screen.dart';
 
-// favorites_screen.dart
 class FavoritesScreen extends StatelessWidget {
-  final List<String> favoriteRecipes;
+  final List<Map<String, dynamic>> favoriteRecipes;
 
   FavoritesScreen({required this.favoriteRecipes});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Favorite Recipes')),
+      appBar: AppBar(title: Text("Favorite Recipes")),
       body: favoriteRecipes.isEmpty
-          ? Center(child: Text('No favorite recipes yet.'))
+          ? Center(
+              child: Text("No favorite recipes yet.",
+                  style: TextStyle(fontSize: 18)))
           : ListView.builder(
               itemCount: favoriteRecipes.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(favoriteRecipes[index]),
+                  title: Text(favoriteRecipes[index]['name']),
+                  subtitle: Text(favoriteRecipes[index]['ingredients']),
                 );
               },
             ),
